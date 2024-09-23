c4cacc3f702e4dd24be1a460ae1ed14e0507fced
jaeyoungpark3250 <48427782+jaeyoungpark3250@users.noreply.github.com>
2024-09-23 22:22:42 +0900

    commit

--- historyservice/src/main/java/finalprjwebtoonservice/infra/HistoriesViewHandler.java
+++ historyservice/src/main/java/finalprjwebtoonservice/infra/HistoriesViewHandler.java
@@ -16,5 +16,47 @@ public class HistoriesViewHandler {
     //<<< DDD / CQRS
     @Autowired
     private HistoriesRepository historiesRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenHistoryCreated_then_CREATE_1(
+        @Payload HistoryCreated historyCreated
+    ) {
+        try {
+            if (!historyCreated.validate()) return;
+
+            // view 객체 생성
+            Histories histories = new Histories();
+            // view 객체에 이벤트의 Value 를 set 함
+            histories.setMyPoint(
+                histories.getMyPoint() +
+                String.valueOf(historyCreated.getMyPoint())
+            );
+            histories.setUserId(historyCreated.getUserId());
+            histories.setStatus(historyCreated.getStatus());
+            // view 레파지 토리에 save
+            historiesRepository.save(histories);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenUserCreated_then_CREATE_2(
+        @Payload UserCreated userCreated
+    ) {
+        try {
+            if (!userCreated.validate()) return;
+
+            // view 객체 생성
+            Histories histories = new Histories();
+            // view 객체에 이벤트의 Value 를 set 함
+            histories.setUserId(userCreated.getUserId());
+            histories.setStatus(userCreated.getStatus());
+            // view 레파지 토리에 save
+            historiesRepository.save(histories);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     //>>> DDD / CQRS
 }
